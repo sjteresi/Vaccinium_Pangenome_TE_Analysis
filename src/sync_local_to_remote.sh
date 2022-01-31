9abacd471fadc37dadab600c5fff47fcab59c9ce
Scott T <teresiscott@gmail.com>
2022-01-31 16:04:11 -0500

    Sync data, create unshortened CDS FASTA, Makefile docs

---
+++ src/sync_local_to_remote.sh
@@ -0,0 +1,3 @@
+#rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Blueberry_Pangenome_TE_Analysis/data --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Blueberry_Pangenome_TE_Analysis/
+
+rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Blueberry_Pangenome_TE_Analysis/results --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Blueberry_Pangenome_TE_Analysis/
